6f955d746634ac0dfad8e4faac6589670a9dc011
TreszkaiT <treszkaitom@gmail.com>
2023-05-20 15:48:01 +0200

    Fix: admin page guard add to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { AuthenticationStoreService } from 'src/app/api/authentication';
+import { AuthenticationStoreServiceImpl } from 'src/app/core/authentication/store/service';
+import { environment } from 'src/environments/environment';
+
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationDataModule } from './core/authentication/data/authentication-data.module';
+import { AuthenticationStoreModule } from './core/authentication/store/authentication-store.module';
 import { SharedModule } from './module/common';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ConfigModule } from './module/config/config.module';
-import { environment } from 'src/environments/environment';
+import { AdminPageGuard } from './page/admin/guard';
 
 @NgModule({
   declarations: [
@@ -16,6 +26,10 @@ import { environment } from 'src/environments/environment';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    // FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule,
     SharedModule,
     StoreModule.forRoot(                                                        // NgRx: 1. Store és Effect elkészítése  és Config betöltése
@@ -29,9 +43,11 @@ import { environment } from 'src/environments/environment';
     ),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
-    ConfigModule
+    ConfigModule,
+    AuthenticationStoreModule,                                              // e nélkül nem megy a login és registration html oldala
+    AuthenticationDataModule                                                // u.a.
   ],
-  providers: [],
+  providers: [ AdminPageGuard,],                                            // e nélkül nem megy az admin html oldala
   bootstrap: [AppComponent]
 })
 export class AppModule { }
